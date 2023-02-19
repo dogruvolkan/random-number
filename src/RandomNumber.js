15ec8480f29d573a081e8376f30480f3a10c63b5
volkan <volkandogru11@gmail.com>
2023-02-19 19:43:58 +0300

    added number option

--- src/RandomNumber.js
+++ src/RandomNumber.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const RandomNumber = () => {
   const [min, setMin] = useState("");
   const [max, setMax] = useState("");
-  const [piece, setPiece] = useState(2);
-  const [showResult, setShowResult] = useState(false);
+  const [piece, setPiece] = useState("");
+  const [selectedRadioValue, setSelectedRadioValue] = useState("");
   const [results, setResults] = useState([]);
+  const [showResult, setShowResult] = useState(false);
 
   const handleMin = (e) => {
     setMin(Number(e.target.value));
@@ -18,10 +19,22 @@ const RandomNumber = () => {
     setPiece(Number(e.target.value));
   };
 
+  const handleOptionChange = (e) => {
+    setSelectedRadioValue(e.target.value);
+  };
+
   const generateNumber = (e) => {
     e.preventDefault();
-    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    setResults(randomNumber);
+
+    for (let i = 0; i < piece; i++) {
+      var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+      results.push(randomNumber);
+      console.log("sonuc0", results);
+      setMin("");
+      setMax("");
+      setPiece("");
+    }
+    console.log("sonuc1", results);
     setShowResult(true);
   };
 
@@ -37,6 +50,7 @@ const RandomNumber = () => {
               type="number"
               onChange={handleMin}
               required
+              className="card-input"
             ></input>
           </div>
           <div>
@@ -46,28 +60,54 @@ const RandomNumber = () => {
               type="number"
               onChange={handleMax}
               required
+              className="card-input"
             ></input>
           </div>
-          {/* <div>
+          <div className="card-radio">
+            <label>
+              <input
+                type="radio"
+                value="option1"
+                checked={selectedRadioValue === "asc"}
+                onChange={handleOptionChange}
+              />
+              ASC
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="option2"
+                checked={selectedRadioValue === "desc"}
+                onChange={handleOptionChange}
+              />
+              DESC
+            </label>
+          </div>
+          <div>
             <label>how many random numbers do you want to generate ?</label>
             <input
               value={piece}
               type="number"
               onChange={handlePiece}
               required
+              className="card-input"
             ></input>
-          </div> */}
-          {
-            <>
-              {showResult ? (
-                <div className="card-result">
-                  <h1>result: {results}</h1>
-                </div>
-              ) : null}
-            </>
-          }
-          <div>
-            <button onClick={generateNumber}>Generate</button>
+          </div>
+
+          {showResult ? (
+            <div className="card-result">
+              {results.map((result, index) => (
+                <h2 key={index}>{result}</h2>
+              ))}
+            </div>
+          ) : null}
+          <div className="buttons">
+            <button className="btn-generate" onClick={generateNumber}>
+              Generate
+            </button>
+            <button className="btn-delete" onClick={() => setResults([])}>
+              Clean
+            </button>
           </div>
         </form>
       </div>
